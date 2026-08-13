b61d817b57233d80b99131156ff66e16dc83271c
A G <A@G>
2026-08-13 20:29:00 +0300

    try 1

--- internal/database/migrations/migrations/000003_identity_id.up.sql
+++ internal/database/migrations/migrations/000003_identity_id.up.sql
@@ -1,4 +1,15 @@
-ALTER TABLE shortener_urls ALTER COLUMN id DROP DEFAULT;
-DROP SEQUENCE IF EXISTS shortener_urls_id_seq;
-ALTER TABLE shortener_urls ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
-SELECT setval(pg_get_serial_sequence('shortener_urls', 'id'), GREATEST((SELECT MAX(id) FROM shortener_urls), 1));
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_attribute
+        WHERE attrelid = 'shortener_urls'::regclass
+          AND attname = 'id'
+          AND attidentity <> ''
+    ) THEN
+        ALTER TABLE shortener_urls ALTER COLUMN id DROP DEFAULT;
+        DROP SEQUENCE IF EXISTS shortener_urls_id_seq;
+        ALTER TABLE shortener_urls ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+        PERFORM setval(pg_get_serial_sequence('shortener_urls', 'id'),
+                       GREATEST((SELECT MAX(id) FROM shortener_urls), 1));
+    END IF;
+END $$;
